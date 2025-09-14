8f5aa709a8831348016b69fe23fc3af87c568ea0
Vivan Poddar <vivanneil@outlook.com>
2025-09-14 18:58:28 -0400

    Fix pdf viewer

--- components/pdf-viewer-with-overlay.tsx
+++ components/pdf-viewer-with-overlay.tsx
@@ -378,7 +378,7 @@ export const PdfViewerWithOverlay: React.FC<PdfViewerWithOverlayProps> = ({
     const { EnterFullScreen } = fullScreenPluginInstance;
 
     return (
-        <div className="overflow-y-scroll max-h-[91vh]">
+        <div className="h-[91vh]">
             <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
                 <div
                     className="bg-white dark:bg-black h-full flex flex-col"
